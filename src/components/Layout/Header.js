3b9f83a02870ee23bb24590eae40553898ce4c93
Zarif <zarifhosen2015@gmail.com>
2023-05-08 18:22:26 +0600

    structure added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+const Header = props => {
+    return <>
+        <header>
+            <h1>Meals</h1>
+            <button>Cart</button>
+        </header>
+        <div></div>
+    </>
+};
+
+export default Header;
