7a082a59a76a764a1f6c2e437be2bcd3b1e00774
Zarif <zarifhosen2015@gmail.com>
2023-05-08 19:06:55 +0600

    meals added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,12 @@
 import mealsImage from '../../assets/meals.jpg';
-import classes from 'Header.module.css'
+import classes from './Header.module.css'
+import HeaderCardButton from './HeaderCardButton';
 
 const Header = props => {
     return <>
         <header className={classes.header}>
             <h1>Meals</h1>
-            <button>Cart</button>
+            <HeaderCardButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="A fine dine with foods." />
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+import MealsSummury from "./MealsSummury"
+import AvailableMeals from "./AvailableMeals"
+const Meals = () => {
+    return <>
+        <MealsSummury />
+        <AvailableMeals />
+    </>
+}
+
+export default Meals
