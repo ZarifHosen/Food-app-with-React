61c037fcf7dacbe994e3713747d0ed955a0b42da
Zarif <zarifhosen2015@gmail.com>
2023-05-09 13:48:19 +0600

    typo bug fixed

--- src/components/Meals/MealsSummury.js
+++ src/components/Meals/MealsSummury.js
@@ -1,6 +1,6 @@
 import classes from './MealsSummury.module.css'
 const MealsSummury = () => {
-    return <section className={classes.summury}>
+    return <section className={classes.summary}>
         <h2>Delicious food, delivered to you.</h2>
         <p> Choose your favorite meal from our broad selection of available meals
             and enjoy a delicious lunch or dinner at home.</p>
