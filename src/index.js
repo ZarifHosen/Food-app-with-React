53be7562f57d3e7745dfc6dd966a4fdc65d487ea
Zarif <zarifhosen2015@gmail.com>
2023-05-25 13:11:06 +0600

    custom scroller inserted via npm and chatgpt help

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { Scrollbar } from 'react-scrollbars-custom';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Scrollbar style={{
+    width: '100%',
+    height: '100vh'
+}}>
+    <App />
+</Scrollbar>);
