31fff353dd39eb0c24ef7510092b22a2847abd7c
Zarif <zarifhosen2015@gmail.com>
2023-05-31 18:36:52 +0600

    bug added, don't know how to fix

--- src/components/Meals/MealsItem/MealsItemForm.js
+++ src/components/Meals/MealsItem/MealsItemForm.js
@@ -1,10 +1,29 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealsItemForm.module.css';
 
-const MealsItemForm = () => {
+const MealsItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enterAmountNumber = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 || enterAmountNumber < 1 || enterAmountNumber > 5) {
+            setAmountIsValid(false);
+            return;
+        }
+        props.onAddToCart(enterAmountNumber)
+    }
+
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input ref={amountInputRef} label="Amount" input={{
+
                 id: 'amount' + Math.random(), //bug fixed
                 type: 'number',
                 min: '1',
@@ -13,6 +32,7 @@ const MealsItemForm = () => {
                 defaultValue: '1'
             }} />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
 
     )
